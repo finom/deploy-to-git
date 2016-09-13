c9189e1ab17d11907b75218ba6c2e62bb077aa8b
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-13 12:15:32 +0300

    chore: Add ESLint

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 #!/usr/bin/env node
-
 const { execSync } = require('child_process');
-const path = require('path');
+
 const variablePrefix = 'npm_package_config_deployToGit_';
 const fields = ['repository', 'branch', 'folder', 'commit', 'script', 'user_name', 'user_email'];
 const cwd = process.cwd();
 const config = {};
 
-for(const field of fields) {
+for (const field of fields) {
     const configVar = process.env[`${variablePrefix}${field}`];
 
-    if(!configVar) {
+    if (!configVar) {
         throw Error(`deployOnGit requires "${field}" field in package config`);
     }
 
     config[field] = configVar.replace(/\$([a-zA-Z0-9_]+)/g, (match, envVarName) => {
         const envVar = process.env[envVarName];
 
-        if(!envVar) {
-            throw Error(`Environment variable "${envVarName}" presented at string "${configVar}" is missing`)
+        if (!envVar) {
+            throw Error(`Environment variable "${envVarName}" presented at string "${configVar}" is missing`);
         }
 
         return envVar;
@@ -32,7 +31,7 @@ execSync(`git clone -b ${config.branch} ${config.repository} ${config.folder} 2>
 console.log(`Starting script ${config.script}...`);
 console.log(execSync(`${config.script}`, { cwd }).toString('utf-8'));
 
-console.log(`Configuring and committing...`);
+console.log('Configuring and committing...');
 execSync(`
     cd ${config.folder} &&
     git config user.email "${config.user_email}" &&
@@ -41,7 +40,7 @@ execSync(`
     git commit --allow-empty -m "${config.commit}" 2>&1
 `, { cwd });
 
-console.log(`Pushing...`);
+console.log('Pushing...');
 execSync(`git push ${config.repository} ${config.branch}`, { cwd });
 
 console.log('Deploying to git is finished.');
